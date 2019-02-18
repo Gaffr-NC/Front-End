c8d4f5649d7347daa8edb7bc3f5f135621d63d75
tcje91 <tcje9117@outlook.com>
2019-02-18 16:59:21 +0000

    implement basic tab navigation

--- gaffr/App.js
+++ gaffr/App.js
@@ -1,33 +1,46 @@
 import React from "react";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import { View } from "react-native";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createMaterialTopTabNavigator
+} from "react-navigation";
 import HomeScreen from "./screens/Home";
 import SwipeScreen from "./screens/SwipeScreen";
 import Login from "./screens/Login";
 
-const AppNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Swiper: SwipeScreen,
-    Login: Login
-  },
-  {
-    initialRouteName: "Login",
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "indianred"
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        fontWeight: "bold"
-      }
-    }
-  }
-);
+// const AppNavigator = createStackNavigator(
+//   {
+//     Home: HomeScreen,
+//     Swiper: SwipeScreen,
+//     Login: Login
+//   },
+//   {
+//     initialRouteName: "Login",
+//     defaultNavigationOptions: {
+//       headerStyle: {
+//         backgroundColor: "indianred"
+//       },
+//       headerTintColor: "#fff",
+//       headerTitleStyle: {
+//         fontWeight: "bold"
+//       }
+//     }
+//   }
+// );
+
+const AppNavigator = createMaterialTopTabNavigator({
+  Home: HomeScreen,
+  Swiper: SwipeScreen,
+  Login: Login
+});
 
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return (
+        <AppContainer />
+    );
   }
 }
