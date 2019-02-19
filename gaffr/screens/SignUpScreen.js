eda8bdac854b0718566ef707fbf858ddb17d5ea2
tcje91 <tcje9117@outlook.com>
2019-02-19 11:04:26 +0000

    add inputs to sign-up screen

--- gaffr/screens/SignUpScreen.js
+++ gaffr/screens/SignUpScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 
 export default class SignUpScreen extends Component {
+    state = {
+        userInput1: "",
+        userInput2: "",
+    }
+
   static navigationOptions = {
     title: "Sign Up"
   };
@@ -10,7 +15,21 @@ export default class SignUpScreen extends Component {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Sign up!</Text>
+        <TextInput style={styles.inputs} value={this.state.userInput} onChangeText={(text) => this.setState({ userInput1: text})}></TextInput>
+        <TextInput style={styles.inputs} value={this.state.userInput} onChangeText={(text) => this.setState({ userInput2: text})}></TextInput>
+        <Button
+          title="SUBMIT"
+          onPress={() => this.props.navigation.navigate("TenantApp")}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    inputs: {
+       backgroundColor: "grey",
+       margin: 10,
+       width: 300,
+    }
+})
