ab6deaf2ecc0ab8b4a426daa694e37255637dae3
tcje91 <tcje9117@outlook.com>
2019-02-21 16:57:03 +0000

    expand on Profile page

--- gaffr/app/src/screens/Profile.tsx
+++ gaffr/app/src/screens/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import { User } from "../utils/interfaces";
 
 export default class Profile extends Component {
   static navigationOptions = {
@@ -7,9 +8,42 @@ export default class Profile extends Component {
   };
 
   render() {
+    const user: User = {
+      email: "landlord@aol.com",
+      name: "Ya Boi Chungus",
+      phone: "0754369543845",
+      property: {
+        bedrooms: 1,
+        city: "manchester",
+        images: ["hi"],
+        propertyType: "gay",
+        petsAllowed: true,
+        smokingAllowed: false,
+        price: 1
+      },
+      preferences: {
+        bedrooms: 1,
+        city: "manchester",
+        propertyType: "gay",
+        petsAllowed: true,
+        smokingAllowed: false,
+        maxPrice: 1,
+        minPrice: 0
+      }
+    };
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Profile!</Text>
+        <Text>Name: {user.name}</Text>
+        <Text>email: {user.email}</Text>
+        <Text>Telephone: {user.phone}</Text>
+        {user.preferences && (
+          <View>
+            <Text>My prefs...</Text>
+            <Text>Bedrooms: {user.preferences.bedrooms}</Text>
+            <Text>City: {user.preferences.city}</Text>
+          </View>
+        )}
       </View>
     );
   }
