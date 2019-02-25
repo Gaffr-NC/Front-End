342fd118f5f9c2d0a6077ea63a2a396a9e648e8a
tcje91 <tcje9117@outlook.com>
2019-02-25 15:02:34 +0000

    fix text input to bottom of screen

--- gaffr/app/src/screens/Chat.tsx
+++ gaffr/app/src/screens/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   View,
   Text,
@@ -6,13 +6,12 @@ import {
   TouchableOpacity,
   ScrollView,
   StyleSheet
-} from 'react-native';
-import { getMatchById, liveListen, sendChatMessage } from '../utils';
-import { DocumentSnapshot } from '@firebase/firestore-types';
-import { ChatMessage, Match } from '../utils/interfaces';
-import { NavigationScreenProp } from 'react-navigation';
-import { FontAwesome } from '@expo/vector-icons';
-
+} from "react-native";
+import { getMatchById, liveListen, sendChatMessage } from "../utils";
+import { DocumentSnapshot } from "@firebase/firestore-types";
+import { ChatMessage, Match } from "../utils/interfaces";
+import { NavigationScreenProp } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
 
 interface Props {
   match: Match;
@@ -23,11 +22,11 @@ interface Props {
 export default class Chat extends Component<Props> {
   state = {
     chatHistory: [],
-    message: '',
+    message: ""
   };
   async componentDidMount() {
-    const match = JSON.parse(this.props.navigation.getParam('match', 'ERROR'));
-    liveListen('matches', match.id, (doc: DocumentSnapshot) => {
+    const match = JSON.parse(this.props.navigation.getParam("match", "ERROR"));
+    liveListen("matches", match.id, (doc: DocumentSnapshot) => {
       const matchData = doc.data();
       this.setState({
         chatHistory: matchData ? matchData.chatHistory : []
@@ -36,8 +35,8 @@ export default class Chat extends Component<Props> {
   }
   sendMessage = async () => {
     const { message } = this.state;
-    const match = JSON.parse(this.props.navigation.getParam('match', 'ERROR'));
-    const userType = this.props.navigation.getParam('userType', 'ERROR');
+    const match = JSON.parse(this.props.navigation.getParam("match", "ERROR"));
+    const userType = this.props.navigation.getParam("userType", "ERROR");
     if (message) {
       const chatMessage: ChatMessage = {
         message,
@@ -45,75 +44,102 @@ export default class Chat extends Component<Props> {
         timestamp: Date.now().toLocaleString()
       };
       sendChatMessage(match.id, chatMessage);
-      this.setState({ message: '' });
+      this.setState({ message: "" });
     }
   };
   render() {
     const { message, chatHistory } = this.state;
-    const userType = this.props.navigation.getParam('userType', 'ERROR');
+    const userType = this.props.navigation.getParam("userType", "ERROR");
     return (
-      <ScrollView style={{ flex: 1 }}>
-        {chatHistory.map((message: ChatMessage) => (
-          <View key={message.timestamp.toString()} style={message.speaker === userType ? styles.userBubble : styles.partnerBubble}>
-            {/* <Text>{message.speaker}</Text> */}
-            <Text style={message.speaker === userType ? styles.user : styles.partner}>{message.message}</Text>
-          </View>
-        ))}
+      <View style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
+          {chatHistory.map((message: ChatMessage) => (
+            <View
+              key={message.timestamp.toString()}
+              style={
+                message.speaker === userType
+                  ? styles.userBubble
+                  : styles.partnerBubble
+              }
+            >
+              {/* <Text>{message.speaker}</Text> */}
+              <Text
+                style={
+                  message.speaker === userType ? styles.user : styles.partner
+                }
+              >
+                {message.message}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
         <View style={styles.inputBar}>
-        <TextInput
-          placeholder="type your message..."
-          value={message}
-          onChangeText={(text: String) => this.setState({ message: text })}
-          style={styles.messageInput}
-        />
-        <TouchableOpacity onPress={() => this.sendMessage()} style={styles.sendButton}><FontAwesome name='arrow-up' size={30}/></TouchableOpacity>
+          <TextInput
+            placeholder="type your message..."
+            value={message}
+            onChangeText={(text: String) => this.setState({ message: text })}
+            style={styles.messageInput}
+          />
+          <TouchableOpacity
+            onPress={() => this.sendMessage()}
+            style={styles.sendButton}
+          >
+            <FontAwesome name="arrow-up" size={25} style={styles.arrow} />
+          </TouchableOpacity>
         </View>
-      </ScrollView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   user: {
-    textAlign: 'right',
-    color: 'white'
+    textAlign: "right",
+    color: "white"
   },
   partner: {
-    textAlign: 'left',
+    textAlign: "left"
   },
   userBubble: {
-    backgroundColor: '#502f4c',
-    width: '70%',
-    alignSelf: 'flex-end',
+    backgroundColor: "#502f4c",
+    width: "70%",
+    alignSelf: "flex-end",
     borderRadius: 20,
     margin: 5,
-    padding: 12,
+    padding: 12
   },
   partnerBubble: {
-    backgroundColor: '#fa214b',
-    width: '70%',
-    alignSelf: 'flex-start',
+    backgroundColor: "#fa214b",
+    width: "70%",
+    alignSelf: "flex-start",
     borderRadius: 20,
     margin: 5,
     padding: 12
   },
   inputBar: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-end'
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "flex-end"
   },
   messageInput: {
-    width: '80%',
+    width: "80%",
     padding: 5,
+    position: "absolute",
+    bottom: 0
   },
   sendButton: {
-    backgroundColor: 'powderblue',
+    backgroundColor: "powderblue",
     padding: 12,
-    borderRadius: 50,
-    width: '20%',
-    borderColor: 'steelblue',
-    borderWidth: 3,
-    alignContent: 'center'
+    borderRadius: 100,
+    width: "20%",
+    borderColor: "steelblue",
+    borderWidth: 2,
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 0
+  },
+  arrow: {
+    textAlign: "center",
+    color: "steelblue"
   }
-})
-
+});
