a77402265f87fcc95d468ab7216ba4cff4a486b2
MandSolo <mandcashin@hotmail.co.uk>
2019-02-25 21:46:42 +0000

    add userType styling

--- gaffr/app/src/screens/UserType.tsx
+++ gaffr/app/src/screens/UserType.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Button } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { NavigationScreenProp } from "react-navigation";
 
 interface Props {
@@ -8,20 +8,54 @@ interface Props {
 export default class UserTypeScreen extends Component<Props> {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button
-          title="I am a tenant"
+      <View style={styles.buttonContainer}>
+        <Text>Which type of profile would you like?</Text>
+        <TouchableOpacity
+          style={styles.tenantButton}
           onPress={() =>
             this.props.navigation.navigate("signUp", { userType: "tenants" })
           }
-        />
-        <Button
-          title="I am a landlord"
+        >
+          <Text>TENANT</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.landlordButton}
           onPress={() =>
             this.props.navigation.navigate("signUp", { userType: "landlords" })
           }
+        >
+          <Text>LANDLORD</Text>
+        </TouchableOpacity>
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    margin: 0,
+    color: "#0B4F6C",
+    backgroundColor: "#dcd1e8"
+  },
+  tenantButton: {
+    backgroundColor: "#502F4C",
+    color: "#ffffff",
+    margin: 5,
+    width: "90%",
+    padding: 5,
+    borderRadius: 10
+  },
+  landlordButton: {
+    backgroundColor: "#502F4C",
+    color: "#ffffff",
+    margin: 5,
+    width: "90%",
+    padding: 5,
+    borderRadius: 10
+  }
+});
